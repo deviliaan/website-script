12d82c9996285c9ff10a725cf0ff9117e7aa681e
deviliaan <blankblankboo@gmail.com>
2024-01-19 10:55:39 +0000

    commit

---
+++ nuxt-app/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<script>
+export default {
+    name: 'Card',
+    props: ['anime']
+}
+</script>
+<style>
+
+</style>
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -5,7 +5,8 @@
 </template>
 
 <script lang="ts" setup>
-
+import Card from '~/components/Card.vue';
+const {data} = await useFetch('/api/recent')
 </script>
 
 <style>
