301083ec6a73be8c5d69edcc43d1d2055e22d11a
deviliaan <blankblankboo@gmail.com>
2024-01-19 09:36:28 +0000

    added

---
+++ nuxt-app/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-slate-800 h-8">
+    <UButton>Wellcome</UButton>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ nuxt-app/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header class="sticky top-0 z-50">
+      <Navbar />
+    </header>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Navbar from "~/components/Navbar.vue";
+</script>
+
+<style>
+
+</style>
